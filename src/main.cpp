cb5c17019ad5e7c1ccbdf23cad74c8c4a039f0a2
Tedster827 <tedster827@gmail.com>
2017-11-26 13:59:51 -0800

    Testing Led and Playback Functions

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,12 @@
 	DigitalIn switch_5(PA_5);
 	DigitalIn	switch_6(PA_6);
 	DigitalIn switch_7(PA_7);
+	DigitalIn Rec_Switch(PB_6);
 //Define output bus for the RGB LED
-	DigitalOut record(PB_10);
+	DigitalOut recordON_Red(PA_2);
+	DigitalOut recordOFF_Green(PA_3);
+// Serial tx, rx connected to the PC via an USB cable
+Serial device(UART_TX, UART_RX);
 
 /*
 Define the PWM speaker output
@@ -22,79 +26,110 @@ void delay(float time) {
 	volatile int i;
 	for (i = 0; i < 1000000*time; i++);
 }
-Serial device(UART_TX, UART_RX);
+
 Timer t;
 
-void shutUP(){
+void shutUP(int &index){
 	speaker = 0;
+	index++;
 }
 
-void playNote(float freq){
+void playNote(float freq, bool playingBack){
 	speaker = 0.7;
 	speaker.period(freq);
-	wait(0.1);
+	if(!playingBack) {
+		wait(0.1);
+	}
 }
 		
+void playBack(float noteSave[], float noteTime[], int marker) {
+	device.baud(9600);
+	int count;
+	for(count = 0; count < marker; count++) {
+		playNote(noteSave[count], true);
+		wait(noteTime[count]);
+	}
+}
+	
 
 /*----------------------------------------------------------------------------
  MAIN function
  *----------------------------------------------------------------------------*/
 
 int main(){
-	
-	
+		float noteSave[100] = {0};
+		float noteTime[100] = {0.1};
+		int noteMarker = 0;
+		
+		bool recording = false;
 	
 	while(1) {
+		recordON_Red = 0;
+		recordOFF_Green = 0;
+		if(!recording) {
+			recordON_Red = 0;
+			recordOFF_Green = 0;
+			delay(1);
+			wait(1);
+		}
 		if (switch_1 == 0) {//If button 1 was pressed
 			//Play Middle C
-			playNote(.00382);
+			playNote(.00382, false);
+			noteSave[noteMarker] = .00382;
 		}
 		if (switch_1 == 1) {
-			shutUP();
+			shutUP(noteMarker);
 		}
 		if (switch_2 == 0) {
 			//Play Middle D
-			playNote(.00341);
+			playNote(.00341, false);
+			noteSave[noteMarker] = .00341;
 		}
 		if (switch_2 == 1) {
-			shutUP();
+			shutUP(noteMarker);
 		}
 		if (switch_3 == 0) {
 			//Play Middle E
-			playNote(.00303);
+			playNote(.00303, false);
+			noteSave[noteMarker] = .00303;
 		}
 		if (switch_3 == 1) {
-			shutUP();
+			shutUP(noteMarker);
 		}
 		if (switch_4 == 0) {
 			//Play Middle F
-			playNote(.00286);
+			playNote(.00286, false);
+			noteSave[noteMarker] = .00286;
 		}
 		if (switch_4 == 1) {
-			shutUP();
+			shutUP(noteMarker);
 		}
 		if (switch_5 == 0) {
 			//Play Middle G
-			playNote(.00255);
+			playNote(.00255, false);
+			noteSave[noteMarker] = .00255;
 		}
 		if (switch_5 == 1) {
-			shutUP();
+			shutUP(noteMarker);
 		}
 		if (switch_6 == 0) {
 			//Play Middle A
-			playNote(.00227);
+			playNote(.00227, false);
+			noteSave[noteMarker] = .00227;
 		}
 		if (switch_6 == 1) {
-			shutUP();
+			shutUP(noteMarker);
 		}
 		if (switch_7 == 0) {
 			//Play Middle B
-			playNote(.00202);
+			playNote(.00202, false);
+			noteSave[noteMarker] = .00202;
 		}
 		if (switch_7 == 1) {
-			shutUP();
+			shutUP(noteMarker);
 		}
 	}
+	playBack(noteSave, noteTime, noteMarker);
 }
 
 // *******************************CSUSM Copyright (c) Tobin and Teddy 2017*************************************
